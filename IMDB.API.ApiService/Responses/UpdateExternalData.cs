aaa5f91b799248d84070de72e9da5054d486d7af
Jason Davis <jasondavis303@gmail.com>
2025-12-30 12:30:56 -0700

    Make TitleType required

--- IMDB.API.ApiService/Responses/UpdateExternalData.cs
+++ IMDB.API.ApiService/Responses/UpdateExternalData.cs
@@ -1,13 +1,10 @@
 ﻿using IMDB.API.ApiService.Data.Models;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IMDB.API.ApiService.Responses;
 
 public class UpdateExternalData : ExternalData
 {
-    [NotMapped]
-    public string? TitleType { get; set; }
+    public required string TitleType { get; set; }
 
-    [NotMapped]
     public bool HasEpisodes { get; set; }
 }
